16e3b4e20b6d75729a1a9dfb5528b7c48b7e3cf7
Dmitriifd <idimka9@gmail.com>
2021-05-13 19:31:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Todo from "./components/Todo";
+import Modal from './components/Modal';
+import Backdrop from "./components/Backdrop";
+
+function App() {
+	
+	return (
+		<div>
+			<h1>My Todos</h1>
+			<Todo text='Todo 1' />
+			<Todo text='Todo 2' />
+			<Modal />
+			<Backdrop />
+		</div>
+		
+	)
+}
+
+export default App
---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+
+
+function Todo(props) {
+
+	function deleteHandler() {
+		console.log('click');
+	}
+
+	return (
+		<div className='card'>
+			<h2>{props.text}</h2>
+			<div className='actions'>
+				<button className='btn' onClick={deleteHandler}>Delete</button>
+			</div>
+			
+		</div>
+		
+	)
+}
+
+export default Todo
